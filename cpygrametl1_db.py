19c07aa3d82a877ed5b5de908a0cc816d2400be4
SimonSkelkjaer <sskelk21@student.aau.dk>
2026-09-15 18:01:20 +0200

    Added data generation for DB plus add pygram pipeline for extracting from DB

---
+++ cpygrametl1_db.py
@@ -0,0 +1,240 @@
+import datetime
+import os
+import time
+
+import psycopg2
+import pygrametl
+from dotenv import load_dotenv
+
+from pygrametl import ConnectionWrapper
+from pygrametl.datasources import MergeJoiningSource, SQLSource
+from pygrametl.tables import (
+    BulkFactTable,
+    CachedDimension,
+    SlowlyChangingDimension,
+)
+
+
+load_dotenv()
+
+username = os.getenv("USERNAME")
+source_database = os.getenv("SOURCE_DATABASE")
+dw_database = os.getenv("DW_DATABASE")
+
+
+# Connection to target DW
+pgconn = psycopg2.connect(
+    host="localhost",
+    dbname=dw_database,
+    user=username,
+)
+
+connection = ConnectionWrapper(pgconn)
+connection.setasdefault()
+connection.execute("SET search_path TO pygrametlexa")
+
+
+# Connections to source database
+sourceconn1 = psycopg2.connect(
+    host="localhost",
+    dbname=source_database,
+    user=username,
+)
+
+sourceconn2 = psycopg2.connect(
+    host="localhost",
+    dbname=source_database,
+    user=username,
+)
+
+
+# Methods
+def pgcopybulkloader(name, atts, fieldsep, rowsep, nullval, filehandle):
+    cursor = pgconn.cursor()
+
+    cursor.copy_from(
+        file=filehandle,
+        table=name,
+        sep=fieldsep,
+        null=str(nullval),
+        columns=atts,
+    )
+
+
+def datehandling(row, namemapping):
+    # This method is called from ensure(row) when the lookup of a date fails.
+    # We have to calculate all date related fields and add them to the row.
+    date = pygrametl.getvalue(row, "date", namemapping)
+
+    (
+        year,
+        month,
+        day,
+        hour,
+        minute,
+        second,
+        weekday,
+        dayinyear,
+        dst,
+    ) = time.strptime(date, "%Y-%m-%d")
+
+    isoyear, isoweek, isoweekday = datetime.date(
+        year,
+        month,
+        day,
+    ).isocalendar()
+
+    row["day"] = day
+    row["month"] = month
+    row["year"] = year
+    row["week"] = isoweek
+    row["weekyear"] = isoyear
+    row["dateid"] = dayinyear + 366 * (year - 1990)
+
+    return row
+
+
+def extractdomaininfo(row):
+    # Take the 'www.domain.org' part from 'http://www.domain.org/page.html'.
+    domaininfo = row["url"].split("/")[-2]
+    row["domain"] = domaininfo
+
+    # Take the top level which is the last part of the domain.
+    row["topleveldomain"] = domaininfo.split(".")[-1]
+
+
+def extractserverinfo(row):
+    # Find the server name from a string like "ServerName/Version".
+    row["server"] = row["serverversion"].split("/")[0]
+
+
+# Dimension and fact table objects
+pagedim = SlowlyChangingDimension(
+    name="page",
+    key="pageid",
+    attributes=[
+        "url",
+        "size",
+        "domain",
+        "topleveldomain",
+        "serverversion",
+        "server",
+        "validfrom",
+        "validto",
+        "version",
+    ],
+    lookupatts=["url"],
+    versionatt="version",
+    fromatt="validfrom",
+    toatt="validto",
+    srcdateatt="lastmoddate",
+    cachesize=-1,
+    prefill=True,
+)
+
+testdim = CachedDimension(
+    name="test",
+    key="testid",
+    attributes=["testname", "testauthor"],
+    lookupatts=["testname"],
+    prefill=True,
+    defaultidvalue=-1,
+)
+
+datedim = CachedDimension(
+    name="date",
+    key="dateid",
+    attributes=[
+        "date",
+        "day",
+        "month",
+        "year",
+        "week",
+        "weekyear",
+    ],
+    lookupatts=["date"],
+    rowexpander=datehandling,
+    prefill=True,
+)
+
+facttbl = BulkFactTable(
+    name="testresults",
+    keyrefs=["pageid", "testid", "dateid"],
+    measures=["errors"],
+    bulkloader=pgcopybulkloader,
+    bulksize=250000,
+)
+
+
+# Data sources
+downloadlog = SQLSource(
+    connection=sourceconn1,
+    query="""
+        SELECT
+            localfile,
+            url,
+            serverversion,
+            size::text AS size,
+            downloaddate::text AS downloaddate,
+            lastmoddate::text AS lastmoddate
+        FROM downloadlog
+        ORDER BY localfile
+    """,
+    cursorarg="downloadlog_cursor",
+    fetchsize=5000,
+)
+
+testresults = SQLSource(
+    connection=sourceconn2,
+    query="""
+        SELECT
+            localfile,
+            test,
+            errors::text AS errors
+        FROM testresults
+        ORDER BY localfile, test
+    """,
+    cursorarg="testresults_cursor",
+    fetchsize=5000,
+)
+
+inputdata = MergeJoiningSource(
+    downloadlog,
+    "localfile",
+    testresults,
+    "localfile",
+)
+
+
+def main():
+    print(time.asctime())
+
+    for row in inputdata:
+        extractdomaininfo(row)
+        extractserverinfo(row)
+
+        row["size"] = pygrametl.getint(row["size"])
+
+        row["pageid"] = pagedim.scdensure(row)
+        row["dateid"] = datedim.ensure(
+            row,
+            {"date": "downloaddate"},
+        )
+        row["testid"] = testdim.lookup(
+            row,
+            {"testname": "test"},
+        )
+
+        facttbl.insert(row)
+
+    connection.commit()
+
+    sourceconn1.close()
+    sourceconn2.close()
+    connection.close()
+
+    print(time.asctime())
+
+
+if __name__ == "__main__":
+    main()
---
+++ datagenerator/datagenerator_db.py
@@ -0,0 +1,199 @@
+import os
+
+import psycopg2
+from dotenv import load_dotenv
+import random
+import datagenerator
+from itertools import islice
+from psycopg2.extras import execute_values
+
+load_dotenv()
+
+
+def create_source_tables(connection):
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS downloadlog (
+                localfile TEXT PRIMARY KEY,
+                url TEXT NOT NULL,
+                serverversion TEXT NOT NULL,
+                size INTEGER NOT NULL,
+                downloaddate DATE NOT NULL,
+                lastmoddate DATE NOT NULL
+            )
+        """)
+
+        cursor.execute("""
+            CREATE TABLE IF NOT EXISTS testresults (
+                localfile TEXT NOT NULL,
+                test TEXT NOT NULL,
+                errors INTEGER NOT NULL,
+                PRIMARY KEY (localfile, test)
+            )
+        """)
+
+    connection.commit()
+
+def generate_download_rows():
+    servers = ["SomeServer/1.0", "SomeServer/2.0", "SuperServer/3.0"]
+    cache = {}
+
+    rnd = random.Random()
+    rnd.seed(1)
+
+    i = 0
+    year = datagenerator.startyear
+
+    for month in range(1, datagenerator.months + 1):
+        urls = datagenerator.generateurls()
+
+        if month > 1 and month % 12 == 1:
+            year += 1
+
+        month_number = 12 if month % 12 == 0 else month % 12
+        downloaddate = "%d-%02d-01" % (year, month_number)
+
+        for url in urls:
+            i += 1
+            localfile = "%08d.tmp" % i
+
+            tmp = rnd.randint(1, 100)
+
+            if tmp <= 100 - datagenerator.changeprob and url in cache:
+                line = cache[url].copy()
+                line[0] = localfile
+                line[4] = downloaddate
+                cache[url] = line
+
+                yield tuple(line)
+                continue
+
+            domain_number = int(url.split(".tl")[0].split("domain")[1])
+            server = servers[domain_number % len(servers)]
+
+            size = rnd.randint(100, 15000)
+
+            tmp = rnd.randint(2, 28)
+
+            if month % 12 == 1:
+                lastmoddate = "%d-12-%02d" % (year - 1, tmp)
+            elif month % 12 == 0:
+                lastmoddate = "%d-11-%02d" % (year, tmp)
+            else:
+                lastmoddate = "%d-%02d-%02d" % (
+                    year,
+                    month % 12 - 1,
+                    tmp,
+                )
+
+            line = [
+                localfile,
+                url,
+                server,
+                size,
+                downloaddate,
+                lastmoddate,
+            ]
+
+            cache[url] = line
+
+            yield tuple(line)
+
+def insert_download_rows(connection, rows):
+    with connection.cursor() as cursor:
+        cursor.executemany(
+            """
+            INSERT INTO downloadlog (
+                localfile,
+                url,
+                serverversion,
+                size,
+                downloaddate,
+                lastmoddate
+            )
+            VALUES (%s, %s, %s, %s, %s, %s)
+            """,
+            rows,
+        )
+
+    connection.commit()
+
+def generate_test_rows(download_rows):
+    for download_row in download_rows:
+        localfile = download_row[0]
+        size = download_row[3]
+        lastmoddate = download_row[5]
+
+        day = int(lastmoddate.split("-")[-1])
+
+        for test_number in range(datagenerator.tests):
+            test = "Test%d" % test_number
+            errors = (test_number * size) % day
+
+            yield (localfile, test, errors)
+
+def insert_test_rows(connection, rows, batch_size=10000):
+    with connection.cursor() as cursor:
+        while True:
+            batch = list(islice(rows, batch_size))
+
+            if not batch:
+                break
+
+            execute_values(
+                cursor,
+                """
+                INSERT INTO testresults (
+                    localfile,
+                    test,
+                    errors
+                )
+                VALUES %s
+                """,
+                batch,
+            )
+
+    connection.commit()
+
+def clear_source_tables(connection):
+    with connection.cursor() as cursor:
+        cursor.execute("TRUNCATE TABLE testresults, downloadlog")
+
+    connection.commit()
+
+def main():
+    username = os.getenv("USERNAME")
+
+    connection = psycopg2.connect(
+        host="localhost",
+        database="pygrametl_source",
+        user=username,
+    )
+
+    print("Connected successfully")
+
+    create_source_tables(connection)
+    clear_source_tables(connection)
+
+    print("Generating download data...")
+
+    download_rows = generate_download_rows()
+    insert_download_rows(connection, download_rows)
+
+    print("Download rows inserted successfully")
+
+    print("Generating test result data...")
+
+    # Fresh generator because the previous one was consumed
+    download_rows = generate_download_rows()
+    test_rows = generate_test_rows(download_rows)
+
+    insert_test_rows(connection, test_rows)
+
+    print("Test result rows inserted successfully")
+
+    connection.close()
+
+
+if __name__ == "__main__":
+    main()
